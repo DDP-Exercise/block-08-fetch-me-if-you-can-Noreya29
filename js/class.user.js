f2c64b7c2df1f9970b8a0ce07fbe5477ce634f26
Noreya29 <s2510892006@fhooe.at>
2026-06-21 19:18:27 +0200

    update homework

--- js/class.user.js
+++ js/class.user.js
@@ -20,3 +20,35 @@
  *      - href="mailto:.." for the email and
  *      - href=".." target="_blank" for the website.
  *  *******************************************************/
+
+export default class User {
+    constructor({id, name, username, email, website}) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.website = website;
+
+        this.posts = [];
+    }
+
+    addPost(post) {
+        this.posts.push(post);
+    }
+
+    render() {
+        return `
+        <div class="user" data-user-id="${this.id}">
+        <h2>${this.name}</h2>
+        <p><strong>Username:</strong> ${this.username}</p>
+        <p><a href="mailto:${this.email}">${this.email}</a></p>
+        <p><a href="https://${this.website}" target="_blank">${this.website}</a></p>
+        
+        <button class="showingPosts" data-user-id="${this.id}">
+        Show posts
+        </button>
+        
+        <div class="posts-container" data-posts-of="1" style="display:none;"></div>
+        </div>`;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -25,5 +25,78 @@
  *    where "1" stands for the posts ID of course.
  *
  *    I believe in...
- *    You - 2026-06-09
+ *    Noreya - 2026-06-21
  *  *******************************************************/
+
+import User from "./class.user.js";
+import Post from "./class.post.js";
+
+const app = document.querySelector("#app");
+
+//loading users
+async function loadUsers() {
+    const fetchUsers = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await fetchUsers.json();
+    return data.map(user => new User(user));
+}
+
+//loading posts & assign user
+async function loadPosts() {
+    const loadPosts = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await loadPosts.json();
+
+    data.forEach(p => {
+        const post = new Post(p);
+        const user = users.find(u => u.id === p.userId);
+        user.addPost(post);
+    })
+}
+
+//rendering users
+function renderUsers(users) {
+    app.innerHTML = "";
+    users.forEach(user => {
+        app.innerHTML += user.render();
+    });
+}
+
+//event delegation
+app.addEventListener("click", async (event) => {
+    //showing user posts
+    if (event.target.classList.contains("showingPosts")) {
+        const userId = event.target.dataset.userId;
+        const container = document.querySelector(`[data-posts-of="${userId}"]`);
+
+        container.style.display = container.style.display === "none" ? "block" : "none";
+
+        //rendering posts
+        const userObj = users.find(u => u.id == userId);
+        container.innerHTML = userObj.posts.map(p => p.render()).join("");
+    }
+
+    //loading comments
+    if (event.target.classList.contains("loadComments")) {
+        const postId = event.target.dataset.postId;
+        const container = document.querySelector(`[data-comments-of="${postId}"]`);
+
+        //only loading if it is empty
+        if (container.innerHTML === "") {
+            const loadingComments = await fetch("https://jsonplaceholder.typicode.com/comments?postId=1");
+            const comments = await loadingComments.json();
+
+            container.innerHTML = comments.map(comment => `<p><strong>${comment.email}</strong>: ${comment.body}</p>`).join("");
+        }
+
+        container.style.display = container.style.display === "none" ? "block" : "none";
+    }
+});
+
+let users = [];
+
+async function init() {
+    users = await loadUsers();
+    await loadPosts(users);
+    renderUsers(users);
+}
+
+init();
